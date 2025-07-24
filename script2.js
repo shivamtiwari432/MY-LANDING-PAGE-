abc803f952ef5788adeb7cfb6e5b5a9f2f060357
SHIVAM TIWARI <146664085+shivamtiwari432@users.noreply.github.com>
2025-07-24 13:02:50 +0530

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,7 @@
+// Toggle navigation menu on small screens
+const menuToggle = document.getElementById('menu-toggle');
+const navMenu = document.getElementById('nav-menu');
+
+menuToggle.addEventListener('click', () => {
+  navMenu.classList.toggle('active');
+});
